408a8403f3a0de25f20ccf5df662e2e0d066b436
mezhanglei <496623925@qq.com>
2021-03-21 10:28:15 +0800

    fix ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 import DragResize from './core';
 export default DragResize;
-export { EventType, LastEventDataType, EventDataType, Direction, Axis, DragResizeProps } from "./type";
+export type { EventType, LastEventDataType, EventDataType, Direction, Axis, DragResizeProps } from "./type";
